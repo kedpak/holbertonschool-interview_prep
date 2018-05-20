71c27df6cf96a69d2cce7febbace5bfe313c2319
Kevin Pak <kedpak@yahoo.com>
2018-05-20 15:29:02 -0700

    Change styling

--- 0x00-linked_list_cycle/0-check_cycle.c
+++ 0x00-linked_list_cycle/0-check_cycle.c
@@ -11,15 +11,17 @@ int check_cycle(listint_t *list)
 {
 
 if (list == NULL)
-return (0);
+  return (0);
+
 listint_t *head = list;
 listint_t *node = list;
-while (node != NULL && head != NULL && node->next != NULL)
+
+ while (node != NULL && node->next != NULL)
 {
-head = head->next;
-node = node->next->next;
-if (head == node)
-return (1);
+  head = head->next;
+  node = node->next->next;
+  if (head == node)
+    return (1);
 }
-return (0);
+ return (0);
 }
