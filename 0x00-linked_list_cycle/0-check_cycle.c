51dda572f9f304d90fd46d7fe0ca807f657b6b5b
Kevin Pak <kedpak@yahoo.com>
2018-05-20 15:09:00 -0700

    fix styling

--- 0x00-linked_list_cycle/0-check_cycle.c
+++ 0x00-linked_list_cycle/0-check_cycle.c
@@ -7,23 +7,19 @@
  * Description: Checks if linked last has a loop cycle
  * Return: 0 on no cycle, 1 on cycle.
  */
-int check_cycle(listint_t *list) {
+int check_cycle(listint_t *list)
+{
 
-  if (list == NULL) {
-    return 0;
-  }
-
-  listint_t *head = list;
-  listint_t *node = list;
-
-  while(node != NULL && head != NULL && node->next != NULL) {
-
-    head = head->next;
-    node = node->next->next;
-    
-    if (head == node) {
-      return 1;
-    }
-  }
-  return 0;
+if (list == NULL)
+return (0);
+listint_t *head = list;
+listint_t *node = list;
+while (node != NULL && head != NULL && node->next != NULL)
+{
+head = head->next;
+node = node->next->next;
+if (head == node)
+return (1);
+}
+return (0);
 }
